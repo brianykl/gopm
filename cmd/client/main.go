cbd01af6bc0fd43204be44e5ce6cc292a32ece1d
brian <brian.yikun.li@gmail.com>
2025-01-24 11:21:52 -0500

    moved grpc client calls to utils and added init support of flags from client side, ntd: add flag stuff as arguments into proto file

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -22,64 +22,30 @@ func main() {
 		fmt.Println("Could not connect to daemon:", err)
 		return
 	}
+
 	defer conn.Close()
 	client := pb.NewProcessManagerClient(conn)
-
 	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	command := os.Args[1]
 	switch command {
 	case "start":
-		if len(os.Args) < 4 {
-			fmt.Println("Usage: client start <name> <cmd> [args...]")
-			return
-		}
-		name := os.Args[2]
-		cmdToRun := os.Args[3]
-		args := []string{}
-		if len(os.Args) > 4 {
-			args = os.Args[4:]
-		}
-		req := &pb.StartRequest{
-			Name:    name,
-			Command: cmdToRun,
-			Args:    args,
-		}
-		res, err := client.StartProcess(ctx, req)
+		err := utils.RunStart(client, ctx, os.Args)
 		if err != nil {
-			fmt.Println("Error:", err)
-			return
+			fmt.Println("error:", err)
 		}
-		fmt.Println(res.Message)
 
 	case "stop":
-		if len(os.Args) < 3 {
-			fmt.Println("Usage: client stop <name>")
-			return
-		}
-		name := os.Args[2]
-		req := &pb.StopRequest{Name: name}
-		res, err := client.StopProcess(ctx, req)
+		err := utils.RunStop(client, ctx, os.Args)
 		if err != nil {
-			fmt.Println("Error:", err)
-			return
+			fmt.Println("error:", err)
 		}
-		fmt.Println(res.Message)
 
 	case "list":
-		req := &pb.ListRequest{}
-		res, err := client.ListProcess(ctx, req)
+		err := utils.RunList(client, ctx, os.Args)
 		if err != nil {
-			fmt.Println("Error:", err)
-			return
-		}
-		if len(res.Processes) == 0 {
-			fmt.Println("No running processes.")
-		} else {
-			for _, p := range res.Processes {
-				fmt.Printf("Name: %s, PID: %d, Status: %s\n", p.Name, p.Pid, p.Status)
-			}
+			fmt.Println("error:", err)
 		}
 
 	default:
--- internal/utils/cli.go
+++ internal/utils/cli.go
@@ -1,10 +1,97 @@
 package utils
 
-import "fmt"
+import (
+	"context"
+	"flag"
+	"fmt"
+
+	pb "github.com/brianykl/gopm/proto"
+)
 
 func Usage() {
-	fmt.Println("Usage: client <start|stop|list> ...")
-	return
+	fmt.Println("usage: client <start|stop|list> ...")
+}
+
+func RunStart(client pb.ProcessManagerClient, ctx context.Context, args []string) error {
+	fs := flag.NewFlagSet("start", flag.ContinueOnError)
+	var autoRestart string
+	fs.StringVar(&autoRestart, "auto-restart", "never", "auto restart policy (never|always|on-failure)")
+
+	// e.g. `client start -auto-restart=always myapp ping google.com`
+	err := fs.Parse(args)
+	if err != nil {
+		return err
+	}
+
+	subcommand := fs.Args()
+	if len(subcommand) < 2 {
+		return fmt.Errorf("usage: client start <flag> <name> <cmd> [args...]")
+	}
+
+	name := subcommand[0]
+	cmdToRun := subcommand[1]
+	procArgs := subcommand[2:]
+	req := &pb.StartRequest{
+		Name:    name,
+		Command: cmdToRun,
+		Args:    procArgs,
+	}
+	res, err := client.StartProcess(ctx, req)
+	if err != nil {
+		return err
+	}
+	fmt.Println(res.Message)
+	return nil
+}
+
+func RunStop(client pb.ProcessManagerClient, ctx context.Context, args []string) error {
+	fs := flag.NewFlagSet("stop", flag.ContinueOnError)
+	var force bool
+	fs.BoolVar(&force, "force", false, "force stop the process")
+
+	err := fs.Parse(args)
+	if err != nil {
+		return err
+	}
+
+	subcommand := fs.Args()
+	if len(subcommand) < 1 {
+		return fmt.Errorf("usage: client stop <flag> <name>")
+	}
+
+	name := subcommand[0]
+	req := &pb.StopRequest{Name: name}
+	res, err := client.StopProcess(ctx, req)
+	if err != nil {
+		return err
+	}
+	fmt.Println(res.Message)
+	return nil
+}
+
+func RunList(client pb.ProcessManagerClient, ctx context.Context, args []string) error {
+	fs := flag.NewFlagSet("list", flag.ContinueOnError)
+	var verbose bool
+	fs.BoolVar(&verbose, "verbose", false, "show more information")
+
+	err := fs.Parse(args)
+	if err != nil {
+		return err
+	}
+
+	req := &pb.ListRequest{}
+	res, err := client.ListProcess(ctx, req)
+	if err != nil {
+		return err
+	}
+	if len(res.Processes) == 0 {
+		fmt.Println("no running processes.")
+	} else {
+		for _, p := range res.Processes {
+			fmt.Printf("name: %s, PID: %d, status: %s\n", p.Name, p.Pid, p.Status)
+		}
+	}
+	return nil
 }
 
 func ParseFlag(args []string) {
